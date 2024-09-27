d00a364ac8af353de7a6a02603d5d4a4817cd286
boyo <boyo16527@gmail.com>
2024-09-27 15:52:48 -0400

    Basic graphviz to visualize the graph

--- engine.c
+++ engine.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <graphviz/cgraph.h>
+#include <graphviz/gvc.h>
 
 // Our scalar node
 struct Value {
@@ -49,6 +51,52 @@ struct Value* mul(struct Value* left, struct Value* right) {
   return product;
 }
 
+void add_node_and_edges(struct Value* node, Agraph_t* g) {
+  char node_name[20]; 
+  snprintf(node_name, sizeof(node_name), "%.4f", node->data);
+
+  Agnode_t* gv_node = agnode(g, node_name, 1);
+
+  Agnode_t** gv_nodes = malloc(sizeof(Agnode_t*) * node->num_children);
+  int num_gv_nodes = 0;
+
+  for (int i = 0; i < node->num_children; i++) {
+    add_node_and_edges(node->children[i], g);
+    char child_name[20];
+    snprintf(child_name, sizeof(child_name), "%.4f", node->children[i]->data);
+
+    Agnode_t* child_gv_node = agnode(g, child_name, 0);
+    gv_nodes[num_gv_nodes] = child_gv_node;
+    num_gv_nodes++;
+  }
+
+  char op_name[3];
+  snprintf(op_name, sizeof(op_name), "%c", node->op);
+
+  Agnode_t* op_node = agnode(g, op_name, 1);
+  agedge(g, gv_node, op_node, NULL, 1);
+
+  for (int i = 0; i < num_gv_nodes; i++) {
+    agedge(g, op_node, gv_nodes[i], NULL, 1);
+  }
+
+  free(gv_nodes);
+}
+
+void visualize_graph(struct Value* root) {
+  GVC_t* gvc = gvContext();
+  Agraph_t* g = agopen("G", Agdirected, NULL);
+
+  add_node_and_edges(root, g);
+
+  gvLayout(gvc, g, "dot");
+  gvRenderFilename(gvc, g, "png", "output.png");
+  gvFreeLayout(gvc, g);
+
+  agclose(g);
+  gvFreeContext(gvc);
+}
+
 
 int main() {
   struct Value* a = initialize_value(4.0);
@@ -61,8 +109,10 @@ int main() {
   repr(c);
   struct Value* d = initialize_value(-3.0);
   repr(d);
-  struct Value* e = mul(c, d);
-  repr(e);
+  struct Value* L = mul(c, d);
+  repr(L);
+
+  visualize_graph(L);
 
   return 0;
 }
