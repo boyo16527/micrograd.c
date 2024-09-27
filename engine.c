41d7d6ab04b37aca69dd9c29b620fc17b32d4011
boyo <boyo16527@gmail.com>
2024-09-27 12:44:46 -0400

    Support basic add functionality

--- engine.c
+++ engine.c
@@ -1,22 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Our scalar node
 struct Value {
   float data;
+  struct Value** children; // Point to an array of Value pointers
+  int num_children;
 };
 
 void repr(struct Value* self) {
-  printf("Value(data=%.4f)\n", self->data);
+  printf("Value(data=%.4f, children=[", self->data);
+  for (int i = 0; i < self->num_children; i++) {
+    if (i > 0) { printf (", "); }
+
+    repr(self->children[i]);
+  }
+  printf("])\n");
 }
 
-struct Value initialize_value(float data) {
-  struct Value v = {data};
+void add_child(struct Value* self, struct Value* child) {
+  self->children[self->num_children] = child;
+  self->num_children++;
+}
+
+struct Value* initialize_value(float data) {
+  struct Value* v = malloc(sizeof(struct Value));
+  v->data = data;
+  v->children = malloc(sizeof(struct Value*) * 2); 
+  v->num_children = 0;
   return v;
 }
 
+struct Value* add(struct Value* left, struct Value* right) {
+  struct Value* sum = initialize_value(left->data + right->data);
+  add_child(sum, left);
+  add_child(sum, right);
+  return sum;
+}
+
+
 int main() {
-  struct Value a = initialize_value(4.0);
-  repr(&a);
+  struct Value* a = initialize_value(4.0);
+  struct Value* b = initialize_value(2.0);
+
+  repr(a);
+  repr(b);
+
+  struct Value* c = add(a, b);
+  repr(c);
 
   return 0;
 }
