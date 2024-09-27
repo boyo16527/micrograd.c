1df718ae2b95f44d5720cc60fb851dce670c9444
boyo <boyo16527@gmail.com>
2024-09-27 13:32:34 -0400

    Support basic mul

--- engine.c
+++ engine.c
@@ -4,12 +4,13 @@
 // Our scalar node
 struct Value {
   float data;
+  char op;
   struct Value** children; // Point to an array of Value pointers
   int num_children;
 };
 
 void repr(struct Value* self) {
-  printf("Value(data=%.4f, children=[", self->data);
+  printf("Value(data=%.4f, op = %c, children=[", self->data, self->op);
   for (int i = 0; i < self->num_children; i++) {
     if (i > 0) { printf (", "); }
 
@@ -27,15 +28,25 @@ struct Value* initialize_value(float data) {
   struct Value* v = malloc(sizeof(struct Value));
   v->data = data;
   v->children = malloc(sizeof(struct Value*) * 2); 
+  v->op = '\0';
   v->num_children = 0;
   return v;
 }
 
 struct Value* add(struct Value* left, struct Value* right) {
   struct Value* sum = initialize_value(left->data + right->data);
+  sum->op = '+';
   add_child(sum, left);
   add_child(sum, right);
   return sum;
+}
+
+struct Value* mul(struct Value* left, struct Value* right) {
+  struct Value* product = initialize_value(left->data * right->data);
+  product->op = '*';
+  add_child(product, left);
+  add_child(product, right);
+  return product;
 }
 
 
@@ -48,6 +59,10 @@ int main() {
 
   struct Value* c = add(a, b);
   repr(c);
+  struct Value* d = initialize_value(-3.0);
+  repr(d);
+  struct Value* e = mul(c, d);
+  repr(e);
 
   return 0;
 }
